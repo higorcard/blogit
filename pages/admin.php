eda11e620fc4ee34b365be8e498c76c4518b24ef
higorcard <higorcard16@gmail.com>
2023-07-27 09:26:32 -0300

    delete modal addition

--- pages/admin.php
+++ pages/admin.php
@@ -11,10 +11,28 @@
 					<label class="form-check-label" for="switch">enabled</label>
 				</div>
 				<a href="pages/edit-post.php" class="ms-auto me-4"><i class="bi bi-pencil fs-5 text-dark-emphasis"></i></a>
-				<a href="#"><i class="bi bi-trash fs-5 text-dark-emphasis"></i></a>
+				<a data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-trash fs-5 text-dark-emphasis"></i></a>
 			</div>
 		</div>
 	</div>
+</div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-4" id="exampleModalLabel">Delete post</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+				<p class="fs-5">Are you sure you want to delete this post?<br> This action <span class="text-decoration-underline">cannot be undone</span>.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-danger">Confirm</button>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php require_once 'partials/footer.php'; ?>
